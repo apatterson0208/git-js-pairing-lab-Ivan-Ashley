3aa94161cb40a5ec023440f91215fff10c385d36
slaynex <irod1510@gmail.com>
2022-09-29 19:44:20 +0000

    12th function completed

--- index.js
+++ index.js
@@ -77,4 +77,13 @@ function largest(intOne, intTwo, intThree){
 function printTime(){
     console.log(new Date().toLocaleTimeString('en-US'));
 }
-printTime();
+//printTime();
+
+function isLeapYear(year){
+    if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+console.log(isLeapYear(2020))
