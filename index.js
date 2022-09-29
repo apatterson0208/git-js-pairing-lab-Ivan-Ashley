36a5e2165c05a3705c9732c747fd58796182dc53
slaynex <irod1510@gmail.com>
2022-09-29 18:44:00 +0000

    Second function completed

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 //Code your solutions in this file
 function fiveToOneHundred(){
-    for (var i = 5; i < 101; i++) {
+    for (let i = 5; i <= 100; i++) {
         console.log(i);
     }
 }
 fiveToOneHundred();
+
+function multiplesOfThree(){
+    for (let i = 3; i <= 100; i += 3) {
+        console.log(i);
+    }
+}
+multiplesOfThree();
